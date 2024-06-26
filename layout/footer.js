557f5c56661df42943040745f78865df46223201
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-26 11:33:39 +0400

    Layout

---
+++ layout/footer.js
@@ -0,0 +1,6 @@
+export const renderFooter = () => {
+  const footer = document.querySelector("footer");
+  footer.innerHTML = `
+      <p>&copy; 2024 Task Manager</p>
+    `;
+};
---
+++ layout/header.js
@@ -0,0 +1,6 @@
+export const renderHeader = () => {
+  const header = document.querySelector("header");
+  header.innerHTML = `
+    <h1>Task Manager</h1>
+    `;
+};
